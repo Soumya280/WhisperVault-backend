98bf00bedcd6639ea15d1640d23dcc9dee042cc6
Soumya <soumyaranjanpatel280@gmail.com>
2025-09-19 01:05:50 +0530

    entity edit

--- WhisperVault/src/main/java/com/whispervault/Entity/Message.java
+++ WhisperVault/src/main/java/com/whispervault/Entity/Message.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,7 +19,7 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer messageId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
